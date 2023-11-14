487a55ccc70dcc0a9e7feb357dca4688cdc81476
Joy S <stepjoyous@gmail.com>
2023-11-13 23:15:56 -0800

    Update pseudocode on user routes. Also added 404/error page.

--- routes/user.js
+++ routes/user.js
@@ -2,11 +2,17 @@ const express = require('express')
 
 const router = express.Router()
 
-// Create user
+// Access "Add User" page
 router.use('/add-user', (req, res, next) => {
     res.send(
         '<form action="/user" method="POST"><input type="text" name="username"/><button type="submit">Submit</button>'
     )
+})
+
+// Create user
+router.post('/user', (req, res, next) => {
+    console.log(req.body)
+    res.redirect('/')
 })
 
 // Read (get) 1 user
--- server.js
+++ server.js
@@ -4,12 +4,19 @@ const PORT = 3000
 
 const app = express()
 
+// Connect Routers
+const userRoutes = require('./routes/user')
+const wheelRoutes = require('./routes/wheel')
+
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // Home Page
-app.use('/', (req, res, next) => {
-    res.send('<h1>Hello from Express!</h1>')
+app.use(wheelRoutes)
+app.use(userRoutes)
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page Not Found</h1>')
 })
 
 // Prints a log once server starts listening
