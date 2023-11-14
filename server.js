59ebc8c46a4af07863c4fdc2edf5cbf886d4dac1
Joy S <stepjoyous@gmail.com>
2023-11-13 22:52:47 -0800

    Create home page basic view.

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const app = express()
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }))
 
+// Home Page
+app.use('/', (req, res, next) => {
+    res.send('<h1>Hello from Express!</h1>')
+})
+
 // Prints a log once server starts listening
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`)
