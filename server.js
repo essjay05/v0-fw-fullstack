905d021dfce84c9b49d31076b4e0bfb3738ed894
Joy S <stepjoyous@gmail.com>
2023-11-13 22:46:10 -0800

    Install bodyParser and incorporate into server. Create pseudocode for plans for user routes.

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express")
+const bodyParser = require("body-parser")
 const PORT = 3000
 
 const app = express()
 
 // Middleware
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // Prints a log once server starts listening
 app.listen(PORT, () => {
