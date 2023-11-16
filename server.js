c261abba6fbe78d71437612db1e8f63a2ac5f947
Joy S <stepjoyous@gmail.com>
2023-11-15 21:23:21 -0800

    Implement 404 page view for anytime any non-valid path is entered.

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require("express")
 const bodyParser = require("body-parser")
 const PORT = 3000
@@ -18,7 +19,7 @@ app.use(wheelRoutes)
 app.use(userRoutes)
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page Not Found</h1>')
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
 // Prints a log once server starts listening
