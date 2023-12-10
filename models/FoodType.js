6dd9ac5941af54eeec51d0d90cd6235d80888d21
Joy S <stepjoyous@gmail.com>
2023-12-09 22:53:30 -0800

    Updated FoodType model to connect to Wheel model.

--- models/FoodType.js
+++ models/FoodType.js
@@ -2,8 +2,12 @@ const mongoose = require('mongoose')
 
 const foodTypeSchema = new mongoose.Schema({
   name: String,
-  favorite: Boolean
-})
+  favorite: Boolean,
+  wheels: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Wheel' /* Wheel Schema */
+  }]
+}, { timestamps: true })
 
 const FoodType = mongoose.model('FoodType', foodTypeSchema)
 
