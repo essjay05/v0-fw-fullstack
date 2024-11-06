64108fb4e9aa06ee4f3f63c2c25e995fb0a59b62
Joy S <stepjoyous@gmail.com>
2024-11-05 22:41:33 -0800

    Create header.

---
+++ components/global/header.js
@@ -0,0 +1,20 @@
+import '../../sass/header.scss'
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Wheel</a>
+          </li>
+          <li>
+            <a href="/customize-wheel">Customize Wheel</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Header;
